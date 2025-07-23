756bbb4ed4b641c628ffee61d048a628f73af792
lushisulka <sulkaengjellushe5@gmail.com>
2025-07-23 18:28:49 +0200

    check task 7

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,10 @@
 export default function getBudgetObject(income, gdp, capita) {
+
     const budget = {
         income,
         gdp,
-        capita,
+        capita
     };
-
-return budget;
+    return budget;
 }
 
