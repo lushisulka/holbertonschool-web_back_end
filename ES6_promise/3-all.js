aa3cb01fbdef13a85f136f1c4e64fb31d490c6a4
lushisulka <sulkaengjellushe5@gmail.com>
2025-07-30 18:35:10 +0200

    check task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,12 +2,10 @@ import { uploadPhoto, createUser } from "./utils.js";
 
 function handleProfileSignup() {
     return Promise.all([uploadPhoto(), createUser()])
-        .then((Response) => {
-            console.log(
-                `${Response[0].body} ${Response[1].firstName} ${Response[1].lastName}`
-            );
+        .then(([photo, user]) => {
+            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
         })
-        .catch(()=> console.log("Signup system offline"));
+        .catch(() => console.log("Signup system offline"));
 }
 
 export default handleProfileSignup;
