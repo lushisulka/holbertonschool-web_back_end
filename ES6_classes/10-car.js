fed7774f20c608646131d476e7ed74b77b239510
lushisulka <sulkaengjellushe5@gmail.com>
2025-08-07 17:31:02 +0000

    esilint changes

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,11 +1,11 @@
-export default class Car{
-    constructor(brand, motor, color){
-        this._brand=brand;
-        this._motor=motor;
-        this._color=color;
-    }
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-    cloneCar(){
-        return Object.create(Object.getPropertyOf(this), Object.getOwnPropertyDescriptor(this))
-    }
+  cloneCar() {
+    return Object.create(Object.getPrototypeOf(this), Object.getOwnPropertyDescriptors(this));
+  }
 }
--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,40 +1,39 @@
-export default class Currency{
-    constructor(code, name){
-        if(typeof code !== 'string'){
-            throw new Error('Code must be a string')
-        }
-
-        if(typeof name !== 'string'){
-            throw new Error('Name must be a string')
-        }
-
-        this._name=name;
-        this._code=code;
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new Error('Code must be a string');
     }
 
-    get code(){
-        return this._code;
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
     }
+    this._name = name;
+    this._code = code;
+  }
 
-    get name(){
-        return this._name;
-    }
+  get code() {
+    return this._code;
+  }
 
-    set code(code){
-        if(typeof code !== 'string'){
-            throw new Error('Code must be a string')
-        }
-        this._code=code;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(name){
-        if(typeof name !== 'string'){
-            throw new Error('Name must be a string')
-        }
-        this._name=name;
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new Error('Code must be a string');
     }
-    
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`;
-      }
+    this._code = code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
